70845123f7e6dd4ac4c2f60df6544756abe03c0a
曾轩 <helloazl@icloud.com>
2015-06-19 17:34:04 +0800

    添加了一些基类定义
    抽象类真是蛮难写的，心里虚虚的

---
+++ SpiderBeast/Base/Filter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpiderBeast.Base
+{
+    /// <summary>
+    /// 筛选器的基础抽象类。筛选器可以遍历整个HTML文档，筛选出符合规则集合（模式）的节点，将目标内容提取出来。
+    /// </summary>
+    abstract class Filter
+    {
+        /// <summary>
+        /// 筛选器的规则集合，满足所有的规则即会被筛选出来。
+        /// </summary>
+        List<Rule> mRuleSet;
+
+
+        /// <summary>
+        /// 用于根据规则遍历全部子节点，并将结果返回。
+        /// </summary>
+        /// <param name="htmlDOM">筛选范围的根节点，会遍历此节点的所有子节点。一般可以考虑将body节点作为根节点传入。</param>
+        /// <returns>筛选后的结果集合</returns>
+        public virtual List<FilterResult> Filt(HtmlAgilityPack.HtmlNodeNavigator htmlDOM) {
+            //TODO: 完成筛选器的Filt虚方法的基本实现
+            
+            return null;
+        }
+    }
+}
---
+++ SpiderBeast/Base/Rule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpiderBeast.Base
+{
+    /// <summary>
+    /// 规则抽象类，代表一种单一的节点模式，提供验证规则的方法
+    /// </summary>
+    abstract class Rule
+    {
+        /// <summary>
+        /// 规则类的检验方法，验证参数节点是否满足设定的规则
+        /// </summary>
+        /// <param name="targetNode">待验证的的节点</param>
+        /// <returns>返回是否满足规则的布尔值</returns>
+        public abstract bool Verify(HtmlAgilityPack.HtmlNodeNavigator targetNode);
+    }
+}

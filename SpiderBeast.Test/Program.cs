ede372669fe0a17ac1d69cadb976825c3e036ad2
AZLisme <helloazl@icloud.com>
2015-06-22 16:43:43 +0800

    增加起点中文网的规则，针对新版阅读器

--- SpiderBeast.Test/Program.cs
+++ SpiderBeast.Test/Program.cs
@@ -18,15 +18,15 @@ namespace SpiderBeast.Test
     {
         static void Main(string[] args)
         {
-            string url = "http://www.daomubiji.com/she-zhao-gui-cheng-xia-01.html";// ;"http://manhua.dmzj.com/"
+            string url = "http://read.qidian.com/BookReaderNew/3463725,84452150.aspx";// ;"http://manhua.dmzj.com/"
 
             string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "1.txt");
 
             SingleHtmlFetch<LinkContentResult> sf = new SingleHtmlFetch<LinkContentResult>(url, path);
-            TypeRule tr = new TypeRule("link");
-            sf.AddFilter(new Filter(tr));
+            //TypeRule tr = new TypeRule("link");
+            sf.AddFilter(new Filter(new QiDianRule()));
             sf.StartFetch();
-            //Console.WriteLine(Uitlity.HtmlUitilty.GetStringByUrl(url));
+            Console.WriteLine(Uitlity.HtmlUitilty.GetStringByUrl(url));
             Console.WriteLine("Ready");
             Console.ReadKey();
         }
---
+++ SpiderBeast/QiDianRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using SpiderBeast.Base;
+using SpiderBeast.Rules;
+
+namespace SpiderBeast
+{
+    public class QiDianRule : Rule
+    {
+        Rule r;
+
+        public override int Priority
+        {
+            get
+            {
+                return 10;
+            }
+        }
+
+        public QiDianRule()
+        {
+            TypeRule typep = new TypeRule("p");
+            TypeRule typediv = new TypeRule("div");
+            AttributeEqualToRule ar = new AttributeEqualToRule("id", "chaptercontent");
+            LogicAndRule logicand = new LogicAndRule(typediv, ar);
+            RelativeFatherRule f = new RelativeFatherRule(logicand);
+            r = (Rule)(new LogicAndRule(typep, f));
+        }
+
+        public override bool Verify(HtmlNode targetNode)
+        {
+            return r.Verify(targetNode);
+        }
+    }
+}

1266982fe600a45f613b85d38fa8eba416dfc568
刘睿华 <lrh3321@126.com>
2015-07-05 13:11:05 +0800

    添加子模块 SharpZipLib 和 添加对Zip文件操作的支持

---
+++ SpiderBeast/Uitlity/CompressUitlity.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ICSharpCode.SharpZipLib;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Checksums;
+
+namespace SpiderBeast.Uitlity
+{
+    /// <summary>
+    /// 提供对zip格式的压缩/解压缩功能的支持
+    /// </summary>
+    public static class CompressUitlity
+    {
+        static string s_ext;
+        /// <summary>
+        /// 设置、获取压缩目录时使用的默认扩展名
+        /// </summary>
+        /// <remarks>默认值为".cbz"</remarks>
+        public static string DefaultExtension { get { return s_ext; }set { s_ext = value; } }
+
+        static Encoding s_encoding;
+        /// <summary>
+        /// 设置、获取解压时对文件路径使用的编码
+        /// </summary>
+        /// <remarks>默认值为操作系统的当前 ANSI 代码页的编码。</remarks>
+        public static Encoding DefaultEncoding { get { return s_encoding; } set { s_encoding = value; } }
+
+        static CompressUitlity()
+        {
+            s_ext = ".cbz";
+            s_encoding = Encoding.Default;
+        }
+
+
+        /// <summary>
+        /// 压缩单个文件
+        /// </summary>
+        /// <param name="fileToZip">要压缩的文件</param>
+        /// <param name="zipedFile">压缩后的文件</param>
+        /// <param name="compressionLevel">压缩等级</param>
+        /// <param name="blockSize">每次写入大小</param>
+        public static void ZipFile(string fileToZip, string zipedFile, int compressionLevel, int blockSize)
+        {
+            //如果文件没有找到，则报错
+            if (!System.IO.File.Exists(fileToZip))
+            {
+                throw new System.IO.FileNotFoundException("指定要压缩的文件: " + fileToZip + " 不存在!");
+            }
+
+            using (System.IO.FileStream ZipFile = System.IO.File.Create(zipedFile))
+            {
+                using (ZipOutputStream ZipStream = new ZipOutputStream(ZipFile))
+                {
+                    using (System.IO.FileStream StreamToZip = new System.IO.FileStream(fileToZip, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                    {
+                        string fileName = fileToZip.Substring(fileToZip.LastIndexOf("\\") + 1);
+
+                        ZipEntry ZipEntry = new ZipEntry(fileName);
+
+                        ZipStream.PutNextEntry(ZipEntry);
+
+                        ZipStream.SetLevel(compressionLevel);
+
+                        byte[] buffer = new byte[blockSize];
+
+                        int sizeRead = 0;
+
+                        try
+                        {
+                            do
+                            {
+                                sizeRead = StreamToZip.Read(buffer, 0, buffer.Length);
+                                ZipStream.Write(buffer, 0, sizeRead);
+                            }
+                            while (sizeRead > 0);
+                        }
+                        catch (System.Exception ex)
+                        {
+                            throw ex;
+                        }
+
+                        StreamToZip.Close();
+                    }
+
+                    ZipStream.Finish();
+                    ZipStream.Close();
+                }
+
+                ZipFile.Close();
+            }
+        }
+
+        /// <summary>
+        /// 压缩单个文件
+        /// </summary>
+        /// <param name="fileToZip">要进行压缩的文件名</param>
+        /// <param name="zipedFile">压缩后生成的压缩文件名</param>
+        public static void ZipFile(string fileToZip, string zipedFile)
+        {
+            //如果文件没有找到，则报错
+            if (!File.Exists(fileToZip))
+            {
+                throw new System.IO.FileNotFoundException("指定要压缩的文件: " + fileToZip + " 不存在!");
+            }
+
+            using (FileStream fs = File.OpenRead(fileToZip))
+            {
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                fs.Close();
+
+                using (FileStream ZipFile = File.Create(zipedFile))
+                {
+                    using (ZipOutputStream ZipStream = new ZipOutputStream(ZipFile))
+                    {
+                        string fileName = fileToZip.Substring(fileToZip.LastIndexOf("\\") + 1);
+                        ZipEntry ZipEntry = new ZipEntry(fileName);
+                        ZipStream.PutNextEntry(ZipEntry);
+                        ZipStream.SetLevel(5);
+
+                        ZipStream.Write(buffer, 0, buffer.Length);
+                        ZipStream.Finish();
+                        ZipStream.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 压缩多层目录
+        /// </summary>
+        /// <param name="strDirectory">The directory.</param>
+        /// <param name="zipedFile">The ziped file.</param>
+        public static void ZipFileDirectory(string strDirectory, string zipedFile)
+        {
+            using (System.IO.FileStream ZipFile = System.IO.File.Create(zipedFile))
+            {
+                using (ZipOutputStream s = new ZipOutputStream(ZipFile))
+                {
+                    ZipSetp(strDirectory, s, "");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 递归遍历目录
+        /// </summary>
+        /// <param name="strDirectory">The directory.</param>
+        /// <param name="s">The ZipOutputStream Object.</param>
+        /// <param name="parentPath">The parent path.</param>
+        private static void ZipSetp(string strDirectory, ZipOutputStream s, string parentPath)
+        {
+            if (strDirectory[strDirectory.Length - 1] != Path.DirectorySeparatorChar)
+            {
+                strDirectory += Path.DirectorySeparatorChar;
+            }
+            Crc32 crc = new Crc32();
+
+            string[] filenames = Directory.GetFileSystemEntries(strDirectory);
+
+            foreach (string file in filenames)// 遍历所有的文件和目录
+            {
+
+                if (Directory.Exists(file))// 先当作目录处理如果存在这个目录就递归Copy该目录下面的文件
+                {
+                    string pPath = parentPath;
+                    pPath += file.Substring(file.LastIndexOf("\\") + 1);
+                    pPath += "\\";
+                    ZipSetp(file, s, pPath);
+                }
+
+                else // 否则直接压缩文件
+                {
+                    //打开压缩文件
+                    using (FileStream fs = File.OpenRead(file))
+                    {
+
+                        byte[] buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, buffer.Length);
+
+                        string fileName = parentPath + file.Substring(file.LastIndexOf("\\") + 1);
+                        ZipEntry entry = new ZipEntry(fileName);
+
+                        entry.DateTime = DateTime.Now;
+                        entry.Size = fs.Length;
+
+                        fs.Close();
+
+                        crc.Reset();
+                        crc.Update(buffer);
+
+                        entry.Crc = crc.Value;
+                        s.PutNextEntry(entry);
+
+                        s.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 解压缩一个 zip 文件。
+        /// </summary>
+        /// <param name="zipedFile">The ziped file.</param>
+        /// <param name="strDirectory">The STR directory.</param>
+        /// <param name="password">zip 文件的密码。</param>
+        /// <param name="overWrite">是否覆盖已存在的文件。</param>
+        public static void UnZip(string zipedFile, string strDirectory, string password, bool overWrite)
+        {
+            UnZip(zipedFile, strDirectory, password, overWrite, CompressUitlity.DefaultEncoding);
+        }
+        /// <summary>
+        /// 解压缩一个 zip 文件。
+        /// </summary>
+        /// <param name="zipedFile">The ziped file.</param>
+        /// <param name="strDirectory">The STR directory.</param>
+        /// <param name="password">zip 文件的密码。</param>
+        /// <param name="overWrite">是否覆盖已存在的文件。</param>
+        public static void UnZip(string zipedFile, string strDirectory, string password, bool overWrite,Encoding e)
+        {
+
+            if (strDirectory == "")
+                strDirectory = Directory.GetCurrentDirectory();
+            if (!strDirectory.EndsWith("\\"))
+                strDirectory = strDirectory + "\\";
+
+            using (ZipInputStream s = new ZipInputStream(File.OpenRead(zipedFile)))
+            {
+                s.Password = password;
+                ZipEntry theEntry;
+
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string directoryName = "";
+                    string pathToZip = "";
+                    pathToZip = theEntry.GetDecodedName(e);
+
+                    if (pathToZip != "")
+                        directoryName = Path.GetDirectoryName(pathToZip) + "\\";
+
+                    string fileName = Path.GetFileName(pathToZip);
+
+                    Directory.CreateDirectory(strDirectory + directoryName);
+
+                    if (fileName != "")
+                    {
+                        if ((File.Exists(strDirectory + directoryName + fileName) && overWrite) || (!File.Exists(strDirectory + directoryName + fileName)))
+                        {
+                            using (FileStream streamWriter = File.Create(strDirectory + directoryName + fileName))
+                            {
+                                int size = 2048;
+                                byte[] data = new byte[2048];
+                                while (true)
+                                {
+                                    size = s.Read(data, 0, data.Length);
+
+                                    if (size > 0)
+                                        streamWriter.Write(data, 0, size);
+                                    else
+                                        break;
+                                }
+                                streamWriter.Close();
+                            }
+                        }
+                    }
+                }
+
+                s.Close();
+            }
+        }
+
+        #region Z
+        public static string GetDecodedName(this ZipEntry entry,Encoding e)
+        {
+            if (e ==Encoding.Default)
+            {
+                return entry.Name;
+            }            
+            return e.GetString(Encoding.Default.GetBytes(entry.Name));
+        }
+
+        #endregion
+    }
+}
